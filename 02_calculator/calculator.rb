9545231236e28559d99b96406968d2be8204a648
onoumenon <tanhuitian@gmail.com>
2019-06-10 17:00:37 +0800

    Add solutions for calculator and simon_says

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,21 @@
-#write your code here
+def add(x, y)
+    x + y
+end
+
+def subtract(x, y)
+    x - y
+end
+
+def sum(numbers)
+    sum = 0
+    numbers.each { |a| sum += a }
+    sum
+end
+
+def factorial(x)
+    if x <= 1
+        1
+    else 
+        x * factorial(x-1)
+    end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(s)
+    s
+end
+
+def shout(s)
+    s.upcase
+end
+
+def repeat(s, times = 2)
+    ([s] * times).join(" ")
+end
+
+def start_of_word(s, n)
+    s[0..n-1]
+end
+
+def first_word(s)
+    s.split(" ")[0]
+end
+
+def titleize(s)
+    words = s.split(" ")
+    words = words.map.with_index do |word, i|
+        if i > 0 && %w(the and over).include?(word)
+            word
+        else
+            word.capitalize
+        end    
+    end
+    words.join(" ")
+end
